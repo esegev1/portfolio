554c089bb7776872ee4ef6278a845cfc392fae67
esegev1 <esegev1@gmail.com>
2025-11-05 01:17:44 -0500

    lab commit

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 
 
 const Intro = () => {
+    /*******************************************************
+    *                  DATA VARIABLES 
+    ******************************************************/
     const intro = [
         `I’m Eric, I’m a data driven Ops and Product leader with deep tech industry experience.`,
         `But above all else, `,
@@ -13,34 +16,63 @@ const Intro = () => {
     const companies = [
         { id: 0, name: 'google', title: 'Product Lead, Payment Systems', facts: [`Supported YouTube's launch of Shorts monetization`, 'Introduced new tool to capture terms from thousands of contracts'] },
         { id: 1, name: 'salesforce', title: 'Sr. Mgr, Quota & capacity Planning', facts: ['Managed team of 12 resposible for sales quotas for over 10k reps', 'Obtained internal funding to build JavaScript/HTML/CSS based tooling for quota planning'] },
-        { id: 2, name: 'facebook', title: 'Sales Operations Analyst', facts: ['Supported sales teams responisble for $XXXM revenue', ''] },
+        { id: 2, name: 'facebook', title: 'Sales Operations Analyst', facts: ['Supported sales teams responisble for $XXXM revenue'] },
         { id: 3, name: 'nielsen', title: 'Associate, Emereging Growth', facts: ['Built pricing tool using JavaScript', 'Partnered with Music start-ups on data products'] },
     ]
 
-    // State - stores the text content
-    const [selectedContent, setSelectedContent] = useState(null);
+    const background = 'Background';
 
-    const handleClick = (content) => {
-        console.log(`id: ${content.target.id}`);
-        console.log(`company: ${companies[content.target.id].name}`);
-        setSelectedContent(experience[content.target.id]);
+    /*******************************************************
+    *                  HANDLER FUNCTIONS  
+    ******************************************************/
+    const [visibility, setVisibility] = useState('none')
+
+    const handleExperience = () => {
+        //if background is hidden show it, otherwise hide it
+        setVisibility(visibility === 'none' ? 'flex' : 'none')
     }
 
-    const companyLinks = companies.map((company) =>
-        <button key={company.id} onClick={handleClick} className="employer" name={company.name}>
-            <img id={company.id} src={`/images/${company.name}.png`} />
-        </button>
-    );
+    const [employer, setEmployer] = useState(companies[0]);
 
-    const experience = companies.map((company) =>
-        <React.Fragment key={company.id}>
-            <h3>{company.title}</h3>
-            <ul>
-                <li>{company.facts[0]}</li>
-                <li>{company.facts[1]}</li>
-            </ul>
-        </React.Fragment>
-    );
+    const handleEmployer = (company) => {
+        setEmployer(company)
+    }
+
+    /*******************************************************
+     *                  STYLE VARIABLES  
+     ******************************************************/
+    const ericStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '10px',
+
+    }
+
+    const expIconStyle = {
+        height: '25px',
+        width: '25px',
+        padding: '7px',
+        borderRadius: '25px',
+        background: 'white',
+        boxShadow: '0 0 10px 4px #006742',
+        marginLeft: '50px',
+        animation: 'pulseShadow 2s infinite alternate'
+    }
+
+    const backgroundStyle = {
+        display: visibility,
+        width: '400px',
+    }
+
+    const employersStyle = {
+        display: 'flex'
+    }
+
+    const employerImgStyle = {
+        height: '45px',
+    }
+
 
     return (
         <section className="aboutMeContainer">
@@ -50,20 +82,33 @@ const Intro = () => {
                     <p id="p1">{intro[0]}</p>
                     <p id="p2">{intro[1]}<span id="builder">{intro[2]}</span>{intro[3]}</p>
                 </section>
-                <div className="eric">
+                <div className="eric" style={ericStyle}>
+                    <img onClick={handleExperience} src='/images/experience.png' style={expIconStyle} />
                     <img src="/images/ericlogo.png" />
                 </div>
-                <div className="background">
-                    <h2 id="backgroundTitle">Background</h2>
-                    <section className="experience">
-                        {companyLinks}
-                    </section>
-                    {/* Display content when a button is clicked */}
-                    {selectedContent && (
-                        <div className="details">
-                            {selectedContent}
-                        </div>
-                    )}
+                <div className="background" style={backgroundStyle}>
+                    <h2 id="backgroundTitle">{background}</h2>
+                    <div style={employersStyle}>
+                        {companies.map((company) => (
+                            <button key={company.id} onClick={() => handleEmployer(company)} className="employer" name={company.name}
+                                style={{
+                                    border: 'none',
+                                    borderBottom: company.id === employer.id ? '2px solid gray' : '2px solid white',
+                                    
+                                    background: 'none',
+                                }}>
+                                <img id={company.id} src={`/images/${company.name}.png`} style={employerImgStyle} />
+                            </button>
+                        ))}
+                    </div>
+                    <div>
+                        <h3>{employer.title}</h3>
+                        <ul>
+                            {employer.facts.map((fact, index) => (
+                                <li key={index}>{fact}</li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </section>
         </section>
